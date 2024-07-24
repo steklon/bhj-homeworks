867429c2c720b4b5bbcbf8d913dcd539686f9273
steklon <steklo_one@mail.ru>
2024-07-25 00:26:32 +1000

    task countdown timer completed

---
+++ js-features/countdown/task.js
@@ -0,0 +1,14 @@
+const timer = document.getElementById("timer");
+
+const timerId = setInterval(() => {
+    timer.textContent = Number(timer.textContent) - 1
+
+    if (Number(timer.textContent) < 0) {
+
+        clearInterval(timerId);
+
+        alert("Вы победили в конкурсе!")
+
+        timer.textContent = 0
+    }
+}, 1000);
