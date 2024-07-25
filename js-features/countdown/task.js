06a57b7cdf77a31e6607353b0aed4a988eb4c9c4
steklon <steklo_one@mail.ru>
2024-07-26 02:05:55 +1000

    extra lines removed

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,14 +1,11 @@
 const timer = document.getElementById("timer");
 
 const timerId = setInterval(() => {
-    timer.textContent = Number(timer.textContent) - 1
+  timer.textContent = Number(timer.textContent) - 1
 
-    if (Number(timer.textContent) < 0) {
-
-        clearInterval(timerId);
-
-        alert("Вы победили в конкурсе!")
-
-        timer.textContent = 0
-    }
+  if (Number(timer.textContent) < 0) {
+    clearInterval(timerId);
+    alert("Вы победили в конкурсе!")
+    timer.textContent = 0
+  }
 }, 1000);
